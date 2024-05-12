cc7082f46d4e09f304dc3971f385641ba2382306
Arturo Manrique <arturomgzm@hotmail.com>
2024-05-12 13:22:22 -0600

    first commit

---
+++ database/migrations/2024_05_12_191956_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('classification'); // e.g., drama, action, suspense
+            $table->date('release_date');
+            $table->text('general_review')->nullable();
+            $table->unsignedInteger('season')->nullable(); // Only applicable for series
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
